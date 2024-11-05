664ecd3c48c71df95af2de16781c54f013340ce1
KatUser <ekaterina.shirokikh@gmail.com>
2024-11-05 18:13:45 +0700

    2. Сервис - Кинотеатр [#504869] 006_dml_insert_into_film_sessions.sql

--- db/scripts/006_dml_insert_into_film_sessions.sql
+++ db/scripts/006_dml_insert_into_film_sessions.sql
@@ -1,9 +1,9 @@
 
 insert into film_sessions (film_id, halls_id, start_time, end_time, price)
 values
-(1, 1, now(), now() + interval '2 hours', 500),
-(2, 1, now() + interval '2 hour 15 minutes', now() + interval '4 hours 15 minutes', 500),
-(3, 1, now() + interval '4 hours 20 minutes', now() + interval '5 hours 50 minutes', 500),
-(1, 2, now() + interval '6 hours 15 minutes', now() + interval '7 hours 15 minutes', 1500),
-(2, 2, now() + interval '7 hours 30 minutes', now() + interval '9 hours', 1500),
-(3, 2, now() + interval '9 hours 15 minutes', now() + interval '10 hours', 1500);
+(1, 1, '2024-09-04 15:00:00', '2024-09-04 16:40:00', 500),
+(2, 1, '2024-09-04 17:00:00', '2024-09-04 18:50:00', 500),
+(3, 1, '2024-09-04 19:00:00', '2024-09-04 20:50:00', 500),
+(1, 2, '2024-09-04 15:00:00', '2024-09-04 16:40:00', 1500),
+(2, 2, '2024-09-04 17:00:00', '2024-09-04 18:50:00', 1500),
+(3, 2, '2024-09-04 19:00:00', '2024-09-04 20:50:00', 1500);
